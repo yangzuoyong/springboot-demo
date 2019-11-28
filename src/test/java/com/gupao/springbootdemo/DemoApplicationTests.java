3d9028f5e4fa8f7632203f8ebe8e04e647f6eec0
yangzuoyong <657914737@qq.com>
2019-11-29 06:25:28 +0800

    'update'

--- src/test/java/com/gupao/springbootdemo/DemoApplicationTests.java
+++ src/test/java/com/gupao/springbootdemo/DemoApplicationTests.java
@@ -1,13 +1,11 @@
-package springbootdemo.demo;
+package com.gupao.springbootdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
